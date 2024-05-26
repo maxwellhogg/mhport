48706de2cf6640977bca41c12e43fe6e46e1b782
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-26 13:56:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+  	<title>Maxy - Web Developer | Portfolio</title>
+  	<meta name="description" content="A simple new HTML5 file for new projects.">
+  	<meta name="author" content="Your name">
+  	<link rel="preconnect" href="https://fonts.googleapis.com">
+  	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  	<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Gruppo&display=swap" rel="stylesheet">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  	<link rel="stylesheet" href="css/common.css">
+  	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<nav>
+		<div class="pre-nav">
+			<div class="container">
+				<div class="pre-nav-container">
+					<div class="pre-nav-contacts">
+						<a class="pre-nav-link" href="">maxy.hogg@googlemail.com</a> |
+						<a class="pre-nav-link" href="">07407293295</a>
+					</div>
+					<div class="pre-nav-socials">
+						<a href="#" class="fa fa-github"></a>
+						<a href="#" class="fa fa-linkedin"></a>
+						<a href="#" class="fa fa-instagram"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="nav">
+			<div class="container">
+				<div class="nav-container">
+					<div class="nav-logo">
+						<a class="nav-logo-link" href="#">Maxy - Web Developer</a>
+					</div>
+					<div class="nav-items">
+						<a class="nav-item" href="#">Home</a>
+						<a class="nav-item" href="#">About</a>
+						<a class="nav-item" href="#">Projects</a>
+						<a class="nav-item nav-item-contact" href="#">Contact</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+	<main>
+		<div class="skills-container">
+		<div class="container">
+		  	<h1>Todo: Main Content</h1>
+			<div class="skills">
+				<div class="progress-bar-container">
+            	<h2>HTML</h2>
+            	<div class="progress-bar">
+                	<span class="percentage html"></span>
+            	</div>
+        		</div>
+				<div class="progress-bar-container">
+            	<h2>CSS</h2>
+            	<div class="progress-bar">
+                	<span class="percentage css"></span>
+            	</div>
+        		</div>
+				<div class="progress-bar-container">
+            	<h2>JavaScript</h2>
+            	<div class="progress-bar">
+                	<span class="percentage javascript"></span>
+            	</div>
+        		</div>
+				<div class="progress-bar-container">
+            	<h2>PHP</h2>
+            	<div class="progress-bar">
+                	<span class="percentage php"></span>
+            	</div>
+        		</div>
+        		<div class="progress-bar-container">
+            	<h2>MySQL</h2>
+            	<div class="progress-bar">
+                	<span class="percentage mysql"></span>
+            	</div>
+        		</div>
+    		</div>
+		</div>
+		</div>
+	</main>
+	<footer class="footer">
+		<div class="container">
+			<div class="footer-container">
+				<div class="footer-col">
+					<div class="nav-logo">
+						<a class="nav-logo-link" href="#">Maxy - Web Developer</a>
+					</div>
+					<div class="footer-socials">
+						<a href="#" class="fa fa-github"></a>
+						<a href="#" class="fa fa-linkedin"></a>
+						<a href="#" class="fa fa-instagram"></a>
+					</div>
+				</div>
+				<div class="footer-col">
+					<div class="footer-contact">
+						<a class="footer-contact-link" href="">maxy.hogg@googlemail.com</a>
+						<a class="footer-contact-link" href="">07407293295</a>
+					</div>
+				</div>
+				<div class="footer-col">
+					<div class="footer-sitemap">
+						<a class="footer-sitemap-item" href="#">Home</a>
+						<a class="footer-sitemap-item" href="#">About</a>
+						<a class="footer-sitemap-item" href="#">Projects</a>
+						<a class="footer-sitemap-item" href="#">Contact</a>
+						<a class="footer-sitemap-item" href="#">Admin</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>

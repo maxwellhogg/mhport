f64403a91306d201b0ef6af3490b5b65c5a9332f
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-26 18:04:33 +0100

    updated main.css

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 
   	<title>Maxy - Web Developer | Portfolio</title>
-  	<meta name="description" content="A simple new HTML5 file for new projects.">
-  	<meta name="author" content="Your name">
+  	<meta name="Maxwell Hogg" content="Portfolio">
   	<link rel="preconnect" href="https://fonts.googleapis.com">
   	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   	<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Gruppo&display=swap" rel="stylesheet">
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  	<link rel="stylesheet" href="css/common.css">
-  	<link rel="stylesheet" href="css/main.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  	
+	<link rel="stylesheet" href="css/common.css">
+  	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<nav>
@@ -35,7 +35,7 @@
 			<div class="container">
 				<div class="nav-container">
 					<div class="nav-logo">
-						<a class="nav-logo-link" href="#">Maxy - Web Developer</a>
+						<a class="nav-logo-link" href="#">Maxy Hogg</a>
 					</div>
 					<div class="nav-items">
 						<a class="nav-item" href="#">Home</a>
@@ -48,42 +48,59 @@
 		</div>
 	</nav>
 	<main>
-		<div class="skills-container">
 		<div class="container">
-		  	<h1>Todo: Main Content</h1>
-			<div class="skills">
-				<div class="progress-bar-container">
-            	<h2>HTML</h2>
-            	<div class="progress-bar">
-                	<span class="percentage html"></span>
-            	</div>
-        		</div>
-				<div class="progress-bar-container">
-            	<h2>CSS</h2>
-            	<div class="progress-bar">
-                	<span class="percentage css"></span>
-            	</div>
-        		</div>
-				<div class="progress-bar-container">
-            	<h2>JavaScript</h2>
-            	<div class="progress-bar">
-                	<span class="percentage javascript"></span>
-            	</div>
-        		</div>
-				<div class="progress-bar-container">
-            	<h2>PHP</h2>
-            	<div class="progress-bar">
-                	<span class="percentage php"></span>
-            	</div>
-        		</div>
-        		<div class="progress-bar-container">
-            	<h2>MySQL</h2>
-            	<div class="progress-bar">
-                	<span class="percentage mysql"></span>
-            	</div>
-        		</div>
-    		</div>
+			<div class="hero-text">
+				<h1 class="hero-header">Hi, I'm Maxy<br />I'm a <span class="highlight-text">Web Developer</span></h1>
+				<div class="separator"></div>
+				<p>I'm a self-taught web developer who is looking to gain some hands-on experience in the field. I am extremely motivated and can offer a solid work ethic, uncompromised honesty, and loyalty which I would bring to every project. My problem-solving skills allow me to tackle complex challenges, and I’m passionate about staying up-to-date with industry trends. I feel that I am equipped to be able to contribute towards creating exceptional and exciting online experiences while adding to my knowledge base. Please get in touch if you think I may be a good fit for your company.</p>
+				<div class="separator"></div>
+				<div class="hero-buttons">
+					<a class="hero-button" href="#">See Portfolio..</a>
+					<a class="hero-button" href="#">Get in touch..</a>
+				</div>
+			</div>
 		</div>
+		<div class="skills-container">
+			<div class="container">
+				<div class="skills">
+					<div class="progress-bar-container">
+						<h2>HTML</h2>
+						<div class="progress-bar">
+							<span class="percentage html"></span>
+						</div>
+					</div>
+					<div class="progress-bar-container">
+						<h2>CSS</h2>
+						<div class="progress-bar">
+							<span class="percentage css"></span>
+						</div>
+					</div>
+					<div class="progress-bar-container">
+						<h2>JavaScript</h2>
+						<div class="progress-bar">
+							<span class="percentage javascript"></span>
+						</div>
+					</div>
+					<div class="progress-bar-container">
+						<h2>PHP</h2>
+						<div class="progress-bar">
+							<span class="percentage php"></span>
+						</div>
+					</div>
+					<div class="progress-bar-container">
+						<h2>MySQL</h2>
+						<div class="progress-bar">
+							<span class="percentage mysql"></span>
+						</div>
+					</div>
+					<div class="progress-bar-container">
+						<h2>Python</h2>
+						<div class="progress-bar">
+							<span class="percentage python"></span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</main>
 	<footer class="footer">
@@ -91,7 +108,7 @@
 			<div class="footer-container">
 				<div class="footer-col">
 					<div class="nav-logo">
-						<a class="nav-logo-link" href="#">Maxy - Web Developer</a>
+						<a class="nav-logo-link" href="#">Maxy Hogg</a>
 					</div>
 					<div class="footer-socials">
 						<a href="#" class="fa fa-github"></a>
